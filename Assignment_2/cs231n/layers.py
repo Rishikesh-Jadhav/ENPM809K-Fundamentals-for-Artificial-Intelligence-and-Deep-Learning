bca0761c70b248db55e5031eeae50b47540a9537
Rishikesh Jadhav <94460336+Rishikesh-Jadhav@users.noreply.github.com>
2023-10-30 19:35:14 -0400

    dropout update

--- Assignment_2/cs231n/layers.py
+++ Assignment_2/cs231n/layers.py
@@ -942,3 +942,70 @@ def spatial_groupnorm_backward(dout, cache):
 
     return dx, dgamma, dbeta
 
+def dropout_forward(x, dropout_param):
+    """
+    Performs the forward pass for (inverted) dropout.
+
+    Inputs:
+    - x: Input data, of any shape
+    - dropout_param: A dictionary with the following keys:
+      - p: Dropout parameter. We keep each neuron output with probability p.
+      - mode: 'test' or 'train'. If the mode is train, then perform dropout;
+        if the mode is test, then just return the input.
+      - seed: Seed for the random number generator. Passing seed makes this
+        function deterministic, which is needed for gradient checking but not
+        in real networks.
+
+    Outputs:
+    - out: Array of the same shape as x.
+    - cache: tuple (dropout_param, mask). In training mode, mask is the dropout
+      mask that was used to multiply the input; in test mode, mask is None.
+    """
+    p, mode = dropout_param["p"], dropout_param["mode"]
+
+    # Set the random seed for reproducibility if provided
+    if "seed" in dropout_param:
+        np.random.seed(dropout_param["seed"])
+
+    mask = None
+    out = None
+
+    if mode == "train":
+        # Generate a binary dropout mask to apply dropout
+        mask = (np.random.rand(*x.shape) < p) / p 
+
+        # Apply dropout to the input x by element-wise multiplication with the mask
+        out = x * mask
+
+    elif mode == "test":
+        # In 'test' mode, return the input as is
+        out = x
+
+    # Cache the dropout parameters and mask for the backward pass
+    cache = (dropout_param, mask)
+    # Ensure the output has the same data type as x and avoid unnecessary copies
+    out = out.astype(x.dtype, copy=False)
+
+    return out, cache
+
+
+def dropout_backward(dout, cache):
+    """
+    Perform the backward pass for (inverted) dropout.
+
+    Inputs:
+    - dout: Upstream derivatives, of any shape
+    - cache: (dropout_param, mask) from dropout_forward.
+    """
+    dropout_param, mask = cache
+    mode = dropout_param["mode"]
+
+    dx = None
+    if mode == "train":
+        # During 'train' mode, undo dropout by applying the mask to the upstream derivatives
+        dx = mask * dout
+
+    elif mode == "test":
+        # In 'test' mode, simply pass the upstream derivatives as is
+        dx = 1 * dout
+    return dx
